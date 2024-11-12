f9a27c17bc3faa26da3a4afe5290933ce575bb7b
Baicheng Wu <clydewu117@outlook.com>
2024-11-12 02:56:02 -0500

    removed ip_basic, enabled overlaying depth points to images

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+from utils import pcd2depth, depth_overlay
+
+cam2_dict = "datasets/data/cam2_img"
+cam3_dict = "datasets/data/cam3_img"
+pcd_dict = "datasets/data/lidar"
+out_depth_cam2 = "datasets/out/cam2_depth"
+out_depth_cam3 = "datasets/out/cam3_depth"
+out_final_cam2 = "datasets/out/cam2_final"
+out_final_cam3 = "datasets/out/cam3_final"
+
+cam2_in_mat = [[31470.1, 0, 2736, 0],
+               [0, 30825, 1824, 0],
+               [0, 0, 1.0, 0]]
+
+cam2_ex_mat = [[0.999933899272713, -0.003245217941172, 0.0111, -0.217316],
+               [0.010881133852003, -0.0108, -0.9999, -0.00038],
+               [0.003544976558669, 0.9998, -0.0112, 0.2076],
+               [0, 0, 0, 1]]
+
+cam3_in_mat = [[31470.1, 0, 2736, 0],
+               [0, 30825, 1824, 0],
+               [0, 0, 1, 0]]
+
+cam3_ex_mat = [[0.999799, -0.00445795, 0.0110814, 0.233303],
+               [0.0108896, -0.00407825, -0.99995, 0.0084903],
+               [0.00468311, 0.999844, -0.00444752, 0.207738],
+               [0, 0, 0, 1]]
+
+width = 5472
+height = 3648
+
+count = 0
+
+print("Starting processing point cloud")
+
+for item in os.listdir(pcd_dict):
+    pcd_path = os.path.join(pcd_dict, item)
+    item_name, extension = os.path.splitext(item)
+    # print(f"Processing {item}")
+    out_path_cam2 = os.path.join(out_depth_cam2, f"{item_name}_depth.png")
+    out_path_cam3 = os.path.join(out_depth_cam3, f"{item_name}_depth.png")
+    #
+    # pcd2depth(pcd_path, width, height, cam2_in_mat, cam2_ex_mat, out_path_cam2)
+    # pcd2depth(pcd_path, width, height, cam3_in_mat, cam3_ex_mat, out_path_cam3)
+    # print(f"Done processing {item}")
+
+    print(f"Overlaying depth points from {item}")
+
+    image_path_cam2 = os.path.join(cam2_dict, f"{item_name}.png")
+    image_path_cam3 = os.path.join(cam3_dict, f"{item_name}.png")
+    out_path_final2 = os.path.join(out_final_cam2, f"{item_name}_final.png")
+    out_path_final3 = os.path.join(out_final_cam3, f"{item_name}_final.png")
+
+    depth_overlay(out_path_cam2, image_path_cam2, out_path_final2)
+    depth_overlay(out_path_cam3, image_path_cam3, out_path_final3)
+    print(f"Done overlaying depth points from {item}")
+
+print("Finished")
---
+++ utils.py
@@ -0,0 +1,60 @@
+import open3d as o3d
+import numpy as np
+import cv2
+import png
+
+
+def pcd2depth(pcd_path, width, height, in_mat, ex_mat, out_path):
+    pcd = o3d.io.read_point_cloud(pcd_path)
+    points = np.asarray(pcd.points)
+
+    depth_map = np.full((height, width), np.inf)
+
+    radius = 5
+
+    for point in points:
+        x, y, z = point
+        point_3d = np.array([x, y, z, 1])
+
+        # project to 2D, ignore the points behind the camera
+        if z > 0:
+            # world to camera
+            point_cam = ex_mat @ point_3d  # (4x4) mult (4x1) = (4x1)
+            # camera to pixel
+            point_px = in_mat @ point_cam  # (3x4) mult (4x1) = (3x1)
+
+            w = point_px[2]
+            u, v = int(point_px[0] / w), int(point_px[1] / w)
+
+            if 0 <= v < height and 0 <= u < width:
+                mask = np.zeros_like(depth_map, dtype=np.uint8)
+                cv2.circle(mask, (u, v), radius, 1, thickness=-1)
+                mask_indices = mask == 1
+                depth_map[mask_indices] = np.minimum(depth_map[mask_indices], w)
+
+    depth_map[np.isinf(depth_map)] = 0
+    depth_map_uint16 = (depth_map * 256).astype(np.uint16)
+
+    with open(out_path, 'wb') as f:
+        writer = png.Writer(width=depth_map_uint16.shape[1],
+                            height=depth_map_uint16.shape[0],
+                            bitdepth=16,
+                            greyscale=True)
+        writer.write(f, depth_map_uint16.tolist())
+
+    print(f"Depth map saved to {out_path}")
+
+
+def depth_overlay(depth_path, img_path, out_path):
+    orig_image = cv2.imread(img_path)
+    depth_image = cv2.imread(depth_path)
+
+    depth_normalized = cv2.normalize(depth_image, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    depth_colored = cv2.applyColorMap(depth_normalized, cv2.COLORMAP_JET)
+
+    alpha = 0.6
+    overlaid_image = cv2.addWeighted(orig_image, 1-alpha, depth_colored, alpha, 0)
+
+    cv2.imwrite(out_path, overlaid_image)
+
+    print(f"Image with depth saved to {out_path}")
