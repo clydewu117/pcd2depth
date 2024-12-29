fd7d362f6c3361719496313aa7b34520e8f91829
Baicheng Wu <clydewu117@outlook.com>
2024-12-28 23:24:14 -0500

    correspondence file added

--- assemble_data.py
+++ assemble_data.py
@@ -13,41 +13,46 @@ des_cam2_dir = os.path.join(des_dir, "cam2_img")
 des_cam3_dir = os.path.join(des_dir, "cam3_img")
 des_lidar_dir = os.path.join(des_dir, "lidar")
 
+log_file_path = os.path.join(des_dir, "correspondence_log.txt")
+
 count = 0
 
 print("Start copying files")
 
-for src_dir in src_dirs:
+with open(log_file_path, "w") as log_file:
+    for src_dir in src_dirs:
 
-    src_cam2_dir = os.path.join(src_dir, "cam2_img")
-    src_cam3_dir = os.path.join(src_dir, "cam3_img")
-    src_lidar_dir = os.path.join(src_dir, "lidar")
-    num_files = len([_ for _ in os.listdir(src_lidar_dir)])
-    print(f"Copying {src_dir}, {num_files} samples in total")
+        src_cam2_dir = os.path.join(src_dir, "cam2_img")
+        src_cam3_dir = os.path.join(src_dir, "cam3_img")
+        src_lidar_dir = os.path.join(src_dir, "lidar")
+        num_files = len([_ for _ in os.listdir(src_lidar_dir)])
+        print(f"Copying {src_dir}, {num_files} samples in total")
 
-    pre_count = count
-    for item in os.listdir(src_lidar_dir):
+        pre_count = count
+        for item in sorted(os.listdir(src_lidar_dir)):
+            src_name = os.path.splitext(item)[0]
+            des_name = str(count)
+            print(f"Copying {src_name} in src to {des_name} in des")
 
-        src_name = os.path.splitext(item)[0]
-        des_name = str(count)
-        print(f"Copying {src_name} in src to {des_name} in des")
+            src_cam2_img_path = os.path.join(src_cam2_dir, f"{src_name}.png")
+            src_cam3_img_path = os.path.join(src_cam3_dir, f"{src_name}.png")
+            src_lidar_path = os.path.join(src_lidar_dir, f"{src_name}.pcd")
 
-        src_cam2_img_path = os.path.join(src_cam2_dir, f"{src_name}.png")
-        src_cam3_img_path = os.path.join(src_cam3_dir, f"{src_name}.png")
-        src_lidar_path = os.path.join(src_lidar_dir, f"{src_name}.pcd")
+            des_cam2_img_path = os.path.join(des_cam2_dir, f"{des_name}.png")
+            des_cam3_img_path = os.path.join(des_cam3_dir, f"{des_name}.png")
+            des_lidar_path = os.path.join(des_lidar_dir, f"{des_name}.pcd")
 
-        des_cam2_img_path = os.path.join(des_cam2_dir, f"{des_name}.png")
-        des_cam3_img_path = os.path.join(des_cam3_dir, f"{des_name}.png")
-        des_lidar_path = os.path.join(des_lidar_dir, f"{des_name}.pcd")
+            shutil.copy2(src_cam2_img_path, des_cam2_img_path)
+            shutil.copy2(src_cam3_img_path, des_cam3_img_path)
+            shutil.copy2(src_lidar_path, des_lidar_path)
 
-        shutil.copy2(src_cam2_img_path, des_cam2_img_path)
-        shutil.copy2(src_cam3_img_path, des_cam3_img_path)
-        shutil.copy2(src_lidar_path, des_lidar_path)
+            log_file.write(f"{src_cam2_img_path} -> {des_cam2_img_path}\n")
+            log_file.write(f"{src_cam3_img_path} -> {des_cam3_img_path}\n")
+            log_file.write(f"{src_lidar_path} -> {des_lidar_path}\n\n")
 
-        count += 1
+            count += 1
 
-    print(f"Finished copying {src_dir}")
-    print(f"{src_dir} are listed from {pre_count} to {count - 1} in {des_dir}")
-    break
+        print(f"Finished copying {src_dir}")
+        print(f"{src_dir} are listed from {pre_count} to {count - 1} in {des_dir}")
 
 print("Finished")
