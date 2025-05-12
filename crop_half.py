4ec3a2df0e9e733f34b86cff7bcdf724b9548eaa
clydewu117 <clydewu117@outlook.com>
2025-05-12 00:35:42 -0400

    fix makedirs

--- crop_half.py
+++ crop_half.py
@@ -19,11 +19,12 @@ disp_dir = os.path.join(data_dir, "disp")
 out_dir = "datasets/test_5_5/mid_half"
 cam2_out_dir = os.path.join(out_dir, "cam2_img")
 cam3_out_dir = os.path.join(out_dir, "cam3_img")
-depth_out_dir = os.path.join(out_dir, "disp")
+disp_out_dir = os.path.join(out_dir, "disp")
 
 os.makedirs(out_dir, exist_ok=True)
 os.makedirs(cam2_out_dir, exist_ok=True)
 os.makedirs(cam3_out_dir, exist_ok=True)
+os.makedirs(disp_out_dir, exist_ok=True)
 
 for item in tqdm(os.listdir(cam2_dir)):
 
@@ -33,7 +34,7 @@ for item in tqdm(os.listdir(cam2_dir)):
 
     cam2_out_path = os.path.join(cam2_out_dir, item)
     cam3_out_path = os.path.join(cam3_out_dir, item)
-    disp_out_path = os.path.join(depth_out_dir, item)
+    disp_out_path = os.path.join(disp_out_dir, item)
 
     crop_mid_half(cam2_img_path, cam2_out_path)
     crop_mid_half(cam3_img_path, cam3_out_path)
