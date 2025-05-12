34cc38b64327c2c02141af50af28e88f41b23530
clydewu117 <clydewu117@outlook.com>
2025-05-12 00:24:05 -0400

    crop mid half

--- crop_half.py
+++ crop_half.py
@@ -3,42 +3,38 @@ import cv2
 from tqdm import tqdm
 
 
-def crop_lower_half(input_path, output_path=None):
-    image = cv2.imread(input_path)
+def crop_mid_half(input_path, output_path):
+    image = cv2.imread(input_path, cv2.IMREAD_UNCHANGED)
     height = image.shape[0]
-    lower_half = image[height // 2:, :]
+    mid_half = image[height//4:3*height//4, :]
 
-    if output_path:
-        cv2.imwrite(output_path, lower_half)
-
-    return lower_half
+    cv2.imwrite(output_path, mid_half)
 
 
-data_dir = "datasets/test_nov"
+data_dir = "datasets/test_5_5/in"
 cam2_dir = os.path.join(data_dir, "cam2_img")
 cam3_dir = os.path.join(data_dir, "cam3_img")
-depth_dir = os.path.join(data_dir, "cam3_depth")
+disp_dir = os.path.join(data_dir, "disp")
 
-out_dir = "datasets/test_2_9_lh"
+out_dir = "datasets/test_5_5/mid_half"
 cam2_out_dir = os.path.join(out_dir, "cam2_img")
 cam3_out_dir = os.path.join(out_dir, "cam3_img")
-depth_out_dir = os.path.join(out_dir, "cam3_depth")
+depth_out_dir = os.path.join(out_dir, "disp")
 
 os.makedirs(out_dir, exist_ok=True)
 os.makedirs(cam2_out_dir, exist_ok=True)
 os.makedirs(cam3_out_dir, exist_ok=True)
 
 for item in tqdm(os.listdir(cam2_dir)):
-    item_name = os.path.splitext(item)[0]
 
     cam2_img_path = os.path.join(cam2_dir, item)
     cam3_img_path = os.path.join(cam3_dir, item)
-    depth_path = os.path.join(depth_dir, f"{item_name}_depth.png")
+    disp_path = os.path.join(disp_dir, item)
 
     cam2_out_path = os.path.join(cam2_out_dir, item)
     cam3_out_path = os.path.join(cam3_out_dir, item)
-    depth_out_path = os.path.join(depth_out_dir, f"{item_name}_depth.png")
+    disp_out_path = os.path.join(depth_out_dir, item)
 
-    crop_lower_half(cam2_img_path, cam2_out_path)
-    crop_lower_half(cam3_img_path, cam3_out_path)
-    crop_lower_half(depth_path, depth_out_path)
+    crop_mid_half(cam2_img_path, cam2_out_path)
+    crop_mid_half(cam3_img_path, cam3_out_path)
+    crop_mid_half(disp_path, disp_out_path)
